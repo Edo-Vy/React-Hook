90438f85bd1354c8809f0ef91a1f51b1592f5b4b
Edo-Vy <tranthaovy8898@gmai.com>
2023-08-28 16:03:16 +0700

    search-done-hook-router

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -7,19 +7,29 @@ let timeout = null;
 export default function Search(props) {
   const [arrProduct, setArrProduct] = useState([]);
   // searchParams : giá trị ban đầu, có thể chấm được nhiều giá trị, chứ không phải 1 giá trị ~ params ,
-  // setSearchParams : gán
+  // searchParams.get('tên params ') : lấy dữ liệu từ url về
+  // setSearchParams : gán, đưa dữ liệu lên url
   const [searchParams, setSearchParams] = useSearchParams(); // TH1
 
-  const [keyword, setKeyWord] = useState(searchParams.get("q")); // navigate(`/search?q=${keyword}`);
+  // const [keyword, setKeyWord] = useState(searchParams.get("q")); // navigate(`/search?q=${keyword}`);
 
   const getApiProductByKeyword = async () => {
-    // call api
+    // call api ( B1 : Kích lần 1)
+    // call api (B2: gọi api sau khi load trang)
     try {
-      const result = await axios({
-        url: `https://shop.cyberlearn.vn/api/Product?keyword=${keyword}`,
-        method: "GET",
-      });
-      setArrProduct(result.data.content);
+      if (searchParams.get("q") !== null) {
+        // gõ trên thanh search (Header) trước sẽ ra dữ liệu trước
+        const result = await axios({
+          url: `https://shop.cyberlearn.vn/api/Product?keyword=${searchParams.get(
+            "q"
+          )}`,
+          method: "GET",
+        });
+        // B3 : Sau khi lấy api thành công  về -> state thay đổi -> giao diện render lại ( kết thúc lần 1)
+        setArrProduct(result.data.content);
+        // Lần 2 ( B6)
+        console.log(result.data.content);
+      }
     } catch (erro) {
       console.log(erro);
     }
@@ -31,35 +41,61 @@ export default function Search(props) {
   };
 
   const handleChange = (e) => {
-    const { id, value } = e.target;
-    setKeyWord(value);
-    // khi người dùng gõ phím setKeyWord
+    // const { id, value } = e.target;
+    // B4 : khi người dùng gõ phím trên thanh Search  thì lấy ra giá trị value gán vào thanh url
+    const { value } = e.target;
+    // setKeyWord(value);
+    // khi người dùng gõ phím setSearchParams thực thi  => function được render lại để kích useEffect chạy
+
+    setSearchParams({
+      // làm thay đổi ?q trên thanh url đồng thời kích useEffect chạy lần 2
+      // q: keyword, // q : tên url  - keyword : state  // navigate(`/search?q=${keyword}`);
+      q: value, // q : tham số gán trên url  - keyword : state  // navigate(`/search?q=${keyword}`);
+    });
   };
 
   useEffect(() => {
+    // search (form)
     /**
      *  Khi người dùng thay đổi state thì nó cũng setKeyWord hoặc thay đổi keyword cũng setKeyWord lại
      */
-    setKeyWord(searchParams.get("q")); // TH1
-    getApiProductByKeyword();
-    if (timeout !== null) {
-      clearTimeout(timeout);
-    }
-  }, [searchParams.get("q")]); // TH1
+    // setKeyWord(searchParams.get("q")); // TH1
+    // getApiProductByKeyword();
+    // Lần 1 (B1 : Khi load trang thì hàm này gọi api 1 lần)
+    timeout = setTimeout(() => {
+      // lần 2 ( B5) :
+
+      // setSearchParams({
+      //   q: keyword, // q : tên url  - keyword : state  // navigate(`/search?q=${keyword}`);
+      // });
+
+      // Vừa search vừa gọi api
+      getApiProductByKeyword();
+    }, 1000); // đợi 1s mới setState chứ ko set liền - tới ưu per
+
+    return () => {
+      // B5 : kết quả clear sau khi gọi api
+      if (timeout !== null) {
+        clearTimeout(timeout); // B7 : clear
+      }
+    };
+  }, [searchParams.get("q")]); // TH1 //  Khi params trên url thay đổi thì hàm này sẽ kích hoạt
 
   useEffect(() => {
     // TH2
     // Nếu state thay đổi thì link params (?q) trên url thay đổi theo
     // setSearchParams : là hàm bất đồng bộ cho nên nó set và sau đó  gọi api là chuyện bình thường,
     // ko thấy độ trễ nhiều lắm còn nếu kĩ có thể để asyn...await
-    timeout = setTimeout(() => {
-      setSearchParams({
-        q: keyword, // q : tên url  - keyword : state  // navigate(`/search?q=${keyword}`);
-      });
-      // Vừa search vừa gọi api
-      getApiProductByKeyword();
-    }, 1000); // đợi 1s mới setState chứ ko set liền - tới ưu per
-  }, [keyword]);
+    // timeout = setTimeout(() => {
+    //   // setSearchParams({
+    //   //   q: keyword, // q : tên url  - keyword : state  // navigate(`/search?q=${keyword}`);
+    //   // });
+    //   // Vừa search vừa gọi api
+    //   getApiProductByKeyword();
+    // }, 1000); // đợi 1s mới setState chứ ko set liền - tới ưu per
+    // }, [keyword]);
+  }, []);
+
   return (
     <form className="container" onSubmit={handleSubmit}>
       <h3>Search</h3>
@@ -70,7 +106,7 @@ export default function Search(props) {
             className="form-control"
             id="keyword"
             onChange={handleChange}
-            value={keyword} // navigate(`/search?q=${keyword}`); //TH1
+            // value={keyword} // navigate(`/search?q=${keyword}`); //TH1
           />
           <button className="btn bg-dark text-white mt-3">Search</button>
         </div>
@@ -110,3 +146,5 @@ export default function Search(props) {
  * state thay đổi (keyword) -> setSearchParams => để xét được url không dùng state được, state chỉ xét được
  * ở dưới component => useEffect
  */
+
+// kỹ thuật debounce search : gõ xong sau 1s thì mới gọi
